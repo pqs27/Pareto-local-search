db5b92f3166889b3718600f8a0c624d6d7a1d92c
pqs27 <peiqisun94@gmail.com>
2021-12-09 11:48:59 +0100

    Update read_file.py

--- read_file.py
+++ read_file.py
@@ -1,6 +1,37 @@
 import matplotlib.pyplot as plt
 
-def read_file(filename):
+
+def read_file(filename):  #  instance  = weight, v1, v2, W
+    f = open(filename, 'r')
+    lines = f.readlines()
+    weight = []
+    v1 = []
+    v2 = []
+    instance = [weight,v1,v2]
+    
+    for line in lines:
+        tline = line.split()
+        if tline[0] == 'i':
+            instance[0].append(int(tline[1]))
+            instance[1].append(int(tline[2]))
+            instance[2].append(int(tline[3]))
+        elif tline[0] == 'W':
+            W = int(tline[1])
+    instance.append(W)
+    return instance
+
+
+def read_exact_file(filename):    
+    f = open(filename, 'r')
+    lines = f.readlines()
+    res = []
+    for line in lines:
+        tline = line.split()
+        res.append((int(tline[0]), int(tline[1])))
+    return res
+
+
+"""def read_file(filename):
     with open(filename, 'r') as f:
         content = [l.split(' ') for l in f.readlines()]
     
@@ -17,7 +48,7 @@ def read_exact_file(filename):
         content = [tuple(map(int, l.split('\t'))) for l in f.readlines()]
     
     return content
-
+"""
 def plot_solution(x, ax=None, color='blue'):
     if ax is None:  fig, ax= plt.subplots()
 
